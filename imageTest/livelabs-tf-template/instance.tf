e44d2c4904af8b8b88390c5561bc03227015da6d
Ley Sylvester <212980592+ley-sylvester@users.noreply.github.com>
2026-03-25 18:01:43 -0500

    terraform

---
+++ imageTest/livelabs-tf-template/instance.tf
@@ -0,0 +1,84 @@
+/*
+********************
+# Copyright (c) 2025 Oracle and/or its affiliates. All rights reserved.
+# by - Rene Fontcha - Oracle LiveLabs Platform Lead
+# Last Updated - 06/28/2022
+********************
+*/
+
+locals {
+  workshop_map = {
+    retail     = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/lq-RHb168OE9MebS7yCZRZWYg81728T2Kl-eJxE8txO5lU-8Wg3xipebfWiKlVYI/n/c4u02/b/livestackbucket/o/retailapp.zip"
+    agent      = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/dYkMDom1qydiWbNNpYEsjpN5YGDt_aRcfLRefFZHkpMj6c1eD013EYoAz9o-8Dub/n/c4u02/b/livestackbucket/o/agentapp.zip"
+  }
+  #public_subnet_ocid = var.public_subnet_ocid
+  normalized_industry = lower(trimspace(var.industry))
+  industry_supported  = contains(keys(local.workshop_map), local.normalized_industry)
+  resolved_default    = local.industry_supported ? local.workshop_map[local.normalized_industry] : ""
+  selected_source     = trimspace(var.custom_workshopfiles)
+  selected_workshop = (
+    local.selected_source != ""
+    ? local.selected_source
+    : local.resolved_default
+  )
+}
+
+data "oci_identity_availability_domain" "ad" {
+    compartment_id = var.ociTenancyOcid
+    ad_number      = 1
+}
+
+resource "oci_core_instance" "llw-hol" {
+  count               = var.instance_count
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.ociCompartmentOcid
+  display_name        = "llw-hol-s${format("%02d", count.index + 1)}-${local.timestamp}"
+  shape               = local.instance_shape
+  metadata = {
+    ssh_authorized_keys = local.ssh_authorized_keys
+    workshopfiles       = local.selected_workshop
+    vncpwd              = random_string.vncpwd.result
+    desktop_guide_url   = var.desktop_guide_url
+    desktop_app1_url    = var.desktop_app1_url
+    desktop_app2_url    = var.desktop_app2_url
+  }
+  depends_on = [oci_core_app_catalog_subscription.mp_image_subscription]
+
+  dynamic "shape_config" {
+    for_each = local.is_flex_shape
+    content {
+      ocpus = var.instance_shape_config_ocpus
+    }
+  }
+
+  create_vnic_details {
+    assign_public_ip = true
+    display_name     = "llw-hol-s${format("%02d", count.index + 1)}-${local.timestamp}"
+    hostname_label   = "llw-hol-s${format("%02d", count.index + 1)}-${local.timestamp}"
+    subnet_id        = local.public_subnet_ocid
+  }
+
+  source_details {
+    source_id   = var.instance_image_id
+    source_type = "image"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      display_name, create_vnic_details[0].display_name, create_vnic_details[0].hostname_label,
+    ]
+    /*precondition {
+      condition     = local.public_subnet_ocid != ""
+      error_message = "Provide a public subnet OCID via either ociPublicSubnetOcid or subnet_public_existing."
+    }
+    precondition {
+      condition     = local.selected_workshop != ""
+      error_message = "Either choose a supported industry (retail or agent) or provide a custom workshop zip URL."
+    }*/
+  }
+}
+
+resource "time_sleep" "wait" {
+  depends_on      = [oci_core_instance.llw-hol]
+  create_duration = var.novnc_delay_sec
+}
---
+++ imageTest/livelabs-tf-template/terraform.tfvars
@@ -0,0 +1,17 @@
+shape               = "VM.Standard.E4.Flex"
+custom_image_ocid   = "ocid1.image.oc1.iad.aaaaaaaa7ocu3cnhlinayh56kw57ytq5kmproazsanskth2vd2u7bfai73ia"
+subnet_id           = "ocid1.subnet.oc1.iad.aaaaaaaal2ykd4lxnu7pvlqdgvorcj2mu7xzkjvcndrr5chkxte2pyq34anq"
+availability_domain = "Uocm:US-ASHBURN-AD-1"
+compartment_ocid    = "ocid1.compartment.oc1..aaaaaaaarpe3ohrzzkor4vf7iziuyzbi7yfh3lkum7hxsrk7ekmwnuzkagda"
+ociTenancyOcid      = "ocid1.tenancy.oc1..aaaaaaaa4jbkwc4jal7ydsvwohnrrq6hyxb2thgqazxv43olgwyboav5defq"
+
+
+region              = "us-ashburn-1"
+
+ssh_public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC0ukDpcbhEI6wM84m1/gN3EspdPGM+nTFHh/d3uRaBSGWuAVn9NMfykEBdWpiMwEk244IvaU3IXD9I+MTEfh4nCH6cxO6Ot+acNQxyv/WtmeuhKJraiDHbIVYTD3xCChDmw05aczEQN2PzswXOAODL7hKZjm4lQ+kke4BRz8pjZmkjdvA3mLG9nXbqg4pDIxzYS2J37ud3z9vD74K8AYqhGk+smzgI6AXVXcYqXLvh6Z5mQEmGLFMV+RmhRHcUjQR871/EI60u60y0FD7IWvuw7DadXRtmOl60LniI6sr5DAJtjf+4NmGCMgKUdAv/YdyV6sp6UApq7gClN1JZ6kKNuIzjvMjAJiw9DuxJyyUW3fI5Na1Nx47C2jcILGZUGedJHFM4pPc7A79zFKvZXs0Mci1ukmLt8uExqWLbrz2x1eJuvwkOVkZ5W1OCgzyYjtKkE//2N5HebvABT5/8BVag1J/d/Uew2/p57QZroE2ddMpWLK4oDTBFNRIc1u7qSjO/G4TQtBhRDSGCW8w0ZRXzrR2nYPNUwvO/sPNkrSoaPKPURD/ay+Aj0FBCA2CWDTZfGKNr/mKrXlJ4aTCBdt8PODsx1qDexlMdduKsVc9Yx2q1Lt8g1WQ8BjSP6vDpouIQiTkLNJNHK+MXs2PL6tz3CX9UT7ehzWBtG5dHUSJA6Q== ley.sylvester@oracle.com"
+
+# OPTION 1: simple user
+industry = "retail"
+
+# OPTION 2: advanced user (uncomment to override)
+# custom_workshopfiles = "https://mybucket/custom.zip"
